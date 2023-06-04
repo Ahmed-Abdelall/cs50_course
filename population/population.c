9c2bd9e1753b8cc6a3ee156be157d178e44892ad
Ahmed Abdelall <ahmedabdelall218@gmail.com>
2023-06-04 20:14:12 +0300

    Poplation Add To Github

---
+++ population/population.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("Start Size: ");
+
+    }
+    while (start < 9);
+
+
+    // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End Size: ");
+
+    }
+    while (end < start);
+
+    // TODO: Calculate number of years until we reach threshold
+    int n = 0;
+
+
+    while (start < end)
+    {
+        start = start + (start / 3) - (start / 4);
+        n ++;
+    }
+
+    // TODO: Print number of years
+    if (start == end)
+    {
+        printf("Years: %i\n", n);
+    }
+    else
+    {
+        printf("Years: %i\n", n);
+    }
+
+
+}
